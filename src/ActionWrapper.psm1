5282ff2bf4f33077e9d55edb1299a8c31e7329a1
f0rk <dummy.github.1945@gmail.com>
2020-06-30 09:44:20 +0900

    test

--- src/ActionWrapper.psm1
+++ src/ActionWrapper.psm1
@@ -7,7 +7,7 @@ function Invoke-Action {
         'issue_comment' { Initialize-PR }
         'schedule' { Initialize-Scheduled }
         'issues' { Initialize-Issue }
-        'push' { Initilaize-Push },
+        'push' { Initilaize-Push }
         default { Write-Log 'Not supported event type' }
     }
 }
