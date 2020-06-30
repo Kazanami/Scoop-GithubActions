733e02a666e0c13fe6f0d79238430eea7cc91e75
f0rk <dummy.github.1945@gmail.com>
2020-06-30 09:39:26 +0900

    Push Event Added

---
+++ src/Action/Push.psm1
@@ -0,0 +1,24 @@
+Join-Path $PSScriptRoot '..\Helpers.psm1' | Import-Module
+
+function Initialize-Push {
+    <#
+    .SYNOPSIS
+        Excavator alternative. Based on schedule execute of auto-pr binary.
+    #>
+    Write-Log 'Push initialized'
+
+    $params = @{
+        'Dir'         = $MANIFESTS_LOCATION
+        'Upstream'    = "${REPOSITORY}:master"
+        'Push'        = $true
+        'SkipUpdated' = [bool] $env:SKIP_UPDATED
+    }
+    if ($env:SPECIAL_SNOWFLAKES) { $params.Add('SpecialSnowflakes', ($env:SPECIAL_SNOWFLAKES -split ',')) }
+
+    & (Join-Path $BINARIES_FOLDER 'auto-pr.ps1') @params
+    # TODO: Post some comment?? Or other way how to publish logs for non collaborators.
+
+    Write-Log 'Push finished'
+}
+
+Export-ModuleMember -Function Initialize-Scheduled
